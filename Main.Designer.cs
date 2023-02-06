d2031b14aec41b008a55d485eb744780ae9179d5
Vitaliy Ceban <vitaly.ceban@gmail.com>
2023-02-06 09:57:11 +0200

    Add project files.

---
+++ Main.Designer.cs
@@ -0,0 +1,103 @@
+﻿
+namespace MonitorAutoBrightness
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.SensorValueLabel = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.BrightnessValueLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 9);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(105, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Sensor value:";
+            // 
+            // SensorValueLabel
+            // 
+            this.SensorValueLabel.Location = new System.Drawing.Point(126, 9);
+            this.SensorValueLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.SensorValueLabel.Name = "SensorValueLabel";
+            this.SensorValueLabel.Size = new System.Drawing.Size(140, 20);
+            this.SensorValueLabel.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 41);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(89, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Brightness:";
+            // 
+            // BrightnessValueLabel
+            // 
+            this.BrightnessValueLabel.Location = new System.Drawing.Point(126, 41);
+            this.BrightnessValueLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.BrightnessValueLabel.Name = "BrightnessValueLabel";
+            this.BrightnessValueLabel.Size = new System.Drawing.Size(165, 20);
+            this.BrightnessValueLabel.TabIndex = 3;
+            this.BrightnessValueLabel.Text = "-";
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(318, 77);
+            this.Controls.Add(this.BrightnessValueLabel);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.SensorValueLabel);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.MaximizeBox = false;
+            this.Name = "Main";
+            this.Text = "Monitor Auto-brightness";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label SensorValueLabel;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label BrightnessValueLabel;
+    }
+}
+
---
+++ Main.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace MonitorAutoBrightness
+{
+    public partial class Main : Form
+    {
+        private int previousValue = 0;
+
+        public Main()
+        {
+            InitializeComponent();
+            var timer = new System.Timers.Timer();
+            timer.Elapsed += Timer_Elapsed;
+            timer.Interval = 1000;
+            timer.Start();
+        }
+
+        private void Timer_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            var sensorValueLabelText = SensorValueLabel.Text;
+            var brightnessValueLabelText = BrightnessValueLabel.Text;
+
+            AdjustBrightness(ref sensorValueLabelText, ref brightnessValueLabelText);
+
+            Invoke((Action)(() =>
+            {
+                SensorValueLabel.Text = sensorValueLabelText;
+                BrightnessValueLabel.Text = brightnessValueLabelText;
+            }));
+        }
+
+        private void AdjustBrightness(ref string sensorValueLabelText, ref string brightnessValueLabelText)
+        {
+            using (var digiSpark = new ArduinoUsbDevice())
+            {
+                if (!digiSpark.IsAvailable)
+                {
+                    sensorValueLabelText = "No device";
+                    brightnessValueLabelText = "-";
+                    return;
+                }
+
+                var buffer = new List<byte>();
+                Thread.Sleep(500);
+                while (digiSpark.ReadByte(out byte value))
+                    buffer.Add(value);
+
+                var bufferAsString = System.Text.Encoding.ASCII.GetString(buffer.ToArray());
+                var match = Regex.Match(bufferAsString, @".+\r\n(\d+)\r\n");
+                if (!match.Success)
+                {
+                    sensorValueLabelText = "Value not found";
+                    brightnessValueLabelText = "-";
+                    return;
+                }
+
+                var newValue = Convert.ToInt32(match.Groups[1].Value);
+                if (newValue == previousValue)
+                    return;
+
+                previousValue = newValue;
+                bool isValueWrong = newValue < 0 || newValue >= 1023;
+                if (isValueWrong)
+                {
+                    sensorValueLabelText = "Wrong value";
+                    brightnessValueLabelText = "-";
+                    return;
+                }
+
+                newValue = 1023 - newValue;
+                sensorValueLabelText = newValue.ToString();
+
+                int brightness = GetBrightnessLevel(newValue);
+                if (brightness == int.MinValue)
+                {
+                    brightnessValueLabelText = "Not found";
+                    return;
+                }
+
+                brightnessValueLabelText = brightness.ToString();
+                var appPath = ConfigurationManager.AppSettings["BrightnessManagerAppExecutablePath"];
+                var appArgs = ConfigurationManager.AppSettings["BrightnessManagerAppArgs"];
+
+                using (Process.Start(new ProcessStartInfo
+                {
+                    FileName = Environment.ExpandEnvironmentVariables(appPath),
+                    Arguments = string.Format(appArgs, brightness)
+                })) { }
+            }
+        }
+
+        private int GetBrightnessLevel(int sensorValue)
+        {
+            // Read every time for "hot reload".
+            var levelsFilePath = Path.Combine(Path.GetDirectoryName(Application.ExecutablePath), "BrightnessLevels.txt");
+            if (!File.Exists(levelsFilePath))
+                return int.MinValue;
+
+            var fileLines = File.ReadAllLines(levelsFilePath);
+            foreach (var line in fileLines)
+            {
+                var lineParts = line.Split(new[] { ' ', '\t' });
+                if (lineParts.Length < 2)
+                    continue;
+                try
+                {
+                    var lineSensorValue = Convert.ToInt32(lineParts[0]);
+                    var lineBrightnessLevel = Convert.ToInt32(lineParts[1]);
+
+                    if (sensorValue <= lineSensorValue)
+                        return lineBrightnessLevel;
+                }
+                catch (FormatException) { }
+            }
+
+            return int.MinValue;
+        }
+    }
+}
